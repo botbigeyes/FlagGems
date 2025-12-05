e7e49e66fccb2ee0ac365ec7be29fc676680798d
root <qjk595391@gmail.com>
2025-12-05 06:56:19 +0000

    fix benchmark bug

--- benchmark/test_select_and_slice_perf.py
+++ benchmark/test_select_and_slice_perf.py
@@ -395,26 +395,44 @@ def gen_indices(input_shape, indices_shape, accumulate):
     return indices
 
 
+def gen_indices_bool(input_shape, indices_shape, accumulate, is_bool):
+    indices = []
+
+    if is_bool:
+        mask_shape = indices_shape[0]
+
+        mask = torch.randint(
+            0, 2, size=mask_shape, dtype=torch.bool, device=flag_gems.device
+        )
+        return [mask]
+
+    else:
+        for i, shape in enumerate(indices_shape):
+            index = np.random.choice(
+                np.arange(input_shape[i]), size=shape, replace=accumulate
+            )
+            indices.append(torch.tensor(index, device=flag_gems.device))
+        return indices
+
+
 def index_put_input_fn(accumulate):
     def inner(shapes, dtype, device):
-        input_shape, indices_shape, values_shape = shapes
-        if dtype == torch.bool:
-            inp = torch.randint(
-                0, 2, input_shape, device=flag_gems.device, requires_grad=False
-            ).to(dtype)
-            values = torch.randint(
-                0, 2, values_shape, device=flag_gems.device, requires_grad=False
-            ).to(dtype)
-        else:
-            inp = torch.randn(
-                input_shape, dtype=dtype, device=flag_gems.device, requires_grad=False
+        input_shape, indices_shape, values_shape, is_bool = shapes
+        inp = torch.randn(
+            input_shape, dtype=dtype, device=flag_gems.device, requires_grad=False
+        )
+
+        indices = gen_indices_bool(input_shape, indices_shape, accumulate, is_bool)
+
+        if is_bool:
+            K = indices[0].sum().item()
+            values = torch.randn(
+                (K,), dtype=dtype, device=flag_gems.device, requires_grad=False
             )
+        else:
             values = torch.randn(
                 values_shape, dtype=dtype, device=flag_gems.device, requires_grad=False
             )
-        indices = gen_indices(input_shape, indices_shape, accumulate)
-        if dtype == torch.bool and accumulate:
-            return
         yield inp, indices, values, accumulate
 
     return inner
@@ -423,11 +441,11 @@ def index_put_input_fn(accumulate):
 class IndexPutAccFalseBenchmark(GenericBenchmark):
     def set_more_shapes(self):
         INDEX_PUT_SHAPE = (
-            ((2**28,), ((2**16,),), (2**16,)),
-            ((32, 32), ((8,), (8,)), (8,)),
-            ((32, 32), ((8,), (2, 8)), (8,)),
-            ((32, 32), ((2, 8),), (32,)),
-            ((1024, 1024), ((64,), (64,)), (64,)),
+            ((2**28,), ((2**16,),), (2**16,), False),
+            ((32, 32), ((8,), (8,)), (8,), False),
+            ((32, 32), ((8,), (2, 8)), (8,), False),
+            ((32, 32), ((2, 8),), (32,), False),
+            ((1024, 1024), ((64,), (64,)), (64,), False),
             (
                 (1024, 1024),
                 (
@@ -438,6 +456,7 @@ class IndexPutAccFalseBenchmark(GenericBenchmark):
                     ),
                 ),
                 (64,),
+                False,
             ),
             (
                 (1024, 1024),
@@ -448,10 +467,15 @@ class IndexPutAccFalseBenchmark(GenericBenchmark):
                     ),
                 ),
                 (1024,),
+                False,
             ),
-            ((512, 512, 512), ((128,), (128,), (128,)), (128,)),
-            ((512, 512, 512), ((2, 128), (128,), (128,)), (128,)),
-            ((512, 512, 512), ((2, 128),), (512,)),
+            ((512, 512, 512), ((128,), (128,), (128,)), (128,), False),
+            ((512, 512, 512), ((2, 128), (128,), (128,)), (128,), False),
+            ((512, 512, 512), ((2, 128),), (512,), False),
+            ((100,), ((100,),), (100,), True),
+            ((32, 32), ((32, 32),), (32, 32), True),
+            ((16, 16, 4), ((16, 16, 4),), (16, 16, 4), True),
+            ((1024, 1024), ((1024, 1024),), (1024 * 1024,), True),
         )
         self.shapes = INDEX_PUT_SHAPE
         return None
@@ -479,39 +503,16 @@ def test_index_put__acc_false_perf():
     bench.run()
 
 
-BOOL_DTYPES = [torch.bool]
-
-
-@pytest.mark.index_put
-def test_index_put_acc_false_bool_perf():
-    bench = IndexPutAccFalseBenchmark(
-        op_name="index_put",
-        torch_op=torch.index_put,
-        input_fn=index_put_input_fn(False),
-        dtypes=BOOL_DTYPES,
-    )
-    bench.run()
-
-
-@pytest.mark.index_put_
-def test_index_put__acc_false_bool_perf():
-    bench = IndexPutAccFalseBenchmark(
-        op_name="index_put_",
-        torch_op=torch.index_put_,
-        input_fn=index_put_input_fn(False),
-        dtypes=BOOL_DTYPES,
-    )
-    bench.run()
-
-
 class IndexPutAccTrueBenchmark(GenericBenchmark):
     def set_more_shapes(self):
         INDEX_PUT_SHAPE = (
-            ((2**28,), ((2**16,),), (2**16,)),
-            ((32, 32), ((8,), (8,)), (8,)),
-            ((1024, 1024), ((64,), (64,)), (64,)),
-            ((512, 512, 512), ((128,), (128,), (128,)), (128,)),
-            ((512, 512, 512), ((2, 128), (2, 128), (2, 128)), (2, 128)),
+            ((2**28,), ((2**16,),), (2**16,), False),
+            ((32, 32), ((8,), (8,)), (8,), False),
+            ((1024, 1024), ((64,), (64,)), (64,), False),
+            ((512, 512, 512), ((128,), (128,), (128,)), (128,), False),
+            ((512, 512, 512), ((2, 128), (2, 128), (2, 128)), (2, 128), False),
+            ((512, 512), ((512, 512),), (512 * 512,), True),
+            ((64, 64, 64), ((64, 64, 64),), (64**3,), True),
         )
         self.shapes = INDEX_PUT_SHAPE
         return None
